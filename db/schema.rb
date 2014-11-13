73189751e4142ed8f1c2aa30e8a741f7f554d8bb
ttttang <tracyt.tang@gmail.com>
2014-11-12 18:51:25 -0800

    Added Description to Organization and Fixtures

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141113004611) do
+ActiveRecord::Schema.define(version: 20141113024451) do
 
   create_table "attendances", force: true do |t|
     t.integer  "user_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20141113004611) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "description"
   end
 
   create_table "users", force: true do |t|
