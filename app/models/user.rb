72b34d983b421a380c944377a5d2ca6afc23ec71
ttttang <tracyt.tang@gmail.com>
2014-11-14 12:25:14 -0800

    Devise Added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 	belongs_to :organization
 	has_many :attendances
 	has_many :events, :through=> :attendances
